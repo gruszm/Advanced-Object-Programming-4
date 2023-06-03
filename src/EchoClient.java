0a54ddea127a2fc419cb839599aef651000e5a1f
gruszm <mariuszgruszczynski11@gmail.com>
2023-06-03 15:00:53 +0200

    More

---
+++ src/EchoClient.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient
+{
+    public static void main(String[] args) throws IOException
+    {
+        Socket socket = new Socket("localhost", 7);
+        Scanner sc_keyb = new Scanner(System.in);
+        Scanner sc_sock = new Scanner(socket.getInputStream());
+        PrintWriter pw = new PrintWriter((socket.getOutputStream()), true);
+        String line_out = "";
+        String line_in = "";
+        while (!line_out.equals("bye"))
+        {
+            System.out.println("Podaj tekst: ");
+            line_out = sc_keyb.nextLine();
+            pw.println(line_out);
+            line_in = sc_sock.nextLine();
+            System.out.println("Serwer odpisal: " + line_in);
+        }
+        socket.close();
+    }
+}
---
+++ src/EchoServer.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer
+{
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket ss = new ServerSocket(7);
+        while (true)
+        {
+            System.out.println("Czekam na klienta");
+            Socket socket = ss.accept();
+            System.out.println("Jest klient. Port: " + socket.getPort());
+            Scanner sc = new Scanner(socket.getInputStream());
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            String line = "";
+            while (!line.equals("bye"))
+            {
+                line = sc.nextLine();
+                pw.println("Serwer: " + line.toUpperCase());
+                System.out.println("Klientowi odsyłam linie: " + line);
+            }
+            socket.close();
+        }
+    }
+}
